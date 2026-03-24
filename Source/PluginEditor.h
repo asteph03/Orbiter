86db9fabecf4f4340f48bf604c7cc2909943dc4d
Aidan Stephenson <122556343+asteph03@users.noreply.github.com>
2026-03-24 12:55:16 -0400

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -22,14 +22,18 @@ static std::array<juce::Colour, 3> themeLightColor { Colour(0xff4a8df7), // STAN
                                                     Colour(0xffcc4141), // ORBIT
                                                     Colour(0xff969696) }; // BYPASS
 
-static std::array<juce::Colour, 3> themeDarkColor { Colour(0xff3f7ad9), // STANDARD
+static std::array<juce::Colour, 3> themeDarkColor { Colour(0xff366cc2), // STANDARD
                                                     Colour(0xffa32727), // ORBIT
-                                                    Colour(0xff808080) }; // BYPASS
+                                                    Colour(0xff737373) }; // BYPASS
+
+static std::array<juce::Colour, 3> levelLightColor { Colour(0xff7aa9f5), // STANDARD
+                                                    Colour(0xffc97979), // ORBIT
+                                                    Colour(0xffc4c4c4) }; // BYPASS
 
 //==============================================================================
 /**
 */
-class CapstonePluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class CapstonePluginAudioProcessorEditor  : public juce::AudioProcessorEditor, public Timer
 {
 public:
     CapstonePluginAudioProcessorEditor (CapstonePluginAudioProcessor&, juce::AudioProcessorValueTreeState&);
@@ -39,13 +43,16 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
     void setTheme(bool o, bool b);
+    void timerCallback() override;
 
 private:
     CapstonePluginAudioProcessor& audioProcessor;
     juce::AudioProcessorValueTreeState& apvts;
     AudioVisualizer& visualizer;
+    VerticalMeter& leftMeter;
+    VerticalMeter& rightMeter;
     
-    bool orbit, bypass;
+    bool orbit, bypass, sync;
     
     FxKnob* mixKnob;
     FxKnob* sizeKnob;
@@ -54,10 +61,14 @@ private:
     FxKnob* stereoKnob;
     FxKnob* sprayKnob;
     FxKnob* reverbKnob;
+    FxKnob* swingKnob;
+    FxKnob* beatKnob;
     OrbitButton* orbitButton;
     EnvelopeButton* envButton;
     BypassButton* bypassButton;
     GainSlider* gainSlider;
+    EQComponent* eqComponent;
+    SyncButton* syncButton;
     
     std::unique_ptr<SliderAttachment> mixAttach;
     std::unique_ptr<SliderAttachment> sizeAttach;
@@ -66,9 +77,12 @@ private:
     std::unique_ptr<SliderAttachment> stereoAttach;
     std::unique_ptr<SliderAttachment> sprayAttach;
     std::unique_ptr<SliderAttachment> reverbAttach;
+    std::unique_ptr<SliderAttachment> swingAttach;
+    std::unique_ptr<SliderAttachment> beatAttach;
     std::unique_ptr<ButtonAttachment> orbitAttach;
     std::unique_ptr<ButtonAttachment> bypassAttach;
     std::unique_ptr<SliderAttachment> gainAttach;
+    std::unique_ptr<ButtonAttachment> syncAttach;
     
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CapstonePluginAudioProcessorEditor)
